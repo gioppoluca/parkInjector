ac089a82ef88d8069d5037d305e5928968ce317e
Luca Gioppo <gioppoluca@libero.it>
2017-10-31 16:41:58 +0100

    missing parts

---
+++ src/test/java/it/csi/bigiot/parkingingest/parkIngester/ParkIngesterApplicationTests.java
@@ -0,0 +1,16 @@
+package it.csi.bigiot.parkingingest.parkIngester;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkIngesterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
